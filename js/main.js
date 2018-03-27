559ae5b50c0412aac6d9673576fe0ee7b5ef5c39
bgutsol <bogdan.gutsol@gmail.com>
2018-03-27 21:51:35 +0300

    add fixes

--- js/main.js
+++ js/main.js
@@ -1,61 +1,41 @@
-const searchScreensByOrder = (idsArray) => {
-  return idsArray.map((item) => {
-    return document.querySelector(`template#${item}`);
-  });
+const mainScreen = document.querySelector(`main.central`);
+const screensArray = document.querySelectorAll(`template`);
+let currentScreenNumber = 0;
+const pressedKeys = {};
+
+const changeScreen = (screenNumber) => {
+  if (screenNumber < 0 || screenNumber >= screensArray.length) {
+    return;
+  }
+
+  mainScreen.innerHTML = screensArray[screenNumber].innerHTML;
+  currentScreenNumber = screenNumber;
 };
 
-document.addEventListener(`DOMContentLoaded`, () => {
-  const screensID = [
-    `greeting`,
-    `rules`,
-    `game-1`,
-    `game-2`,
-    `game-3`,
-    `stats`
-  ];
-  const screensArray = searchScreensByOrder(screensID);
-  let currentScreenNumber = 0;
-  const pressedKeys = {};
+const handleDocumentKeyPress = function (e) {
+  pressedKeys[e.keyCode] = (e.type === `keydown`);
 
-  const changeScreen = (screenNumber) => {
-    if (screenNumber < 0 || screenNumber >= screensArray.length) {
-      return;
-    }
-    const mainScreen = document.querySelector(`main.central`);
-    const currScreen = document.getElementById(screensArray[screenNumber].id);
-    mainScreen.innerHTML = currScreen.innerHTML;
-    currentScreenNumber = screenNumber;
-  };
+  if (pressedKeys[18] && pressedKeys[39] || pressedKeys[37]) {
+    e.preventDefault();
+    let nextScreenNumber = currentScreenNumber;
 
-  const handleWindowOnload = () => {
-    changeScreen(0);
-  };
-
-  const handleDocumentKeyPress = function (e) {
-    pressedKeys[e.keyCode] = (e.type === `keydown`);
-
-    if (pressedKeys[18] && pressedKeys[39] || pressedKeys[37]) {
-      e.preventDefault();
-      let nextScreenNumber = currentScreenNumber;
-
-      if (pressedKeys[39]) {
-        if (nextScreenNumber === screensArray.length - 1) {
-          return;
-        }
-        nextScreenNumber++;
+    if (pressedKeys[39]) {
+      if (nextScreenNumber === screensArray.length - 1) {
+        return;
       }
-
-      if (pressedKeys[37]) {
-        if (nextScreenNumber === 0) {
-          return;
-        }
-        nextScreenNumber--;
-      }
-
-      changeScreen(nextScreenNumber);
+      nextScreenNumber++;
     }
-  };
 
-  window.onload = handleWindowOnload;
-  document.onkeydown = document.onkeyup = handleDocumentKeyPress;
-});
+    if (pressedKeys[37]) {
+      if (nextScreenNumber === 0) {
+        return;
+      }
+      nextScreenNumber--;
+    }
+
+    changeScreen(nextScreenNumber);
+  }
+};
+
+changeScreen(0);
+document.onkeydown = document.onkeyup = handleDocumentKeyPress;
